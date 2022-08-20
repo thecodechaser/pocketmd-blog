fc580093f7f09206628f33c4fb4856ed1ab53e31
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-08-21 01:24:27 +0530

    Install apollo graphql

--- comps/posts.tsx
+++ comps/posts.tsx
@@ -1,16 +1,12 @@
-import { P, H2 } from "../styles/comps/Posts.styled";
+import { P, H2 } from '../styles/comps/Posts.styled'
 
 const Posts = () => {
   return (
     <div>
-      <P>
-        Discover more about mens health
-      </P>
-      <H2> 
-        Explore the PocketMD Journal
-      </H2>
+      <P>Discover more about mens health</P>
+      <H2>Explore the PocketMD Journal</H2>
     </div>
   )
 }
 
-export default Posts;
+export default Posts
--- styles/comps/Posts.styled.ts
+++ styles/comps/Posts.styled.ts
@@ -1,15 +1,15 @@
 import styled from 'styled-components'
 
-const P =  styled.p`
-margin-top: 100px;
-color: #CC835C;
-text-align: center;
-font-weight: 500;
+const P = styled.p`
+  margin-top: 100px;
+  color: #cc835c;
+  text-align: center;
+  font-weight: 500;
 `
 
 const H2 = styled.h2`
-font-size: 50px;
-text-align: center;
+  font-size: 50px;
+  text-align: center;
 `
 
-export {P, H2}
+export { P, H2 }
