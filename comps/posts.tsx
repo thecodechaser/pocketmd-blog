01983907af1f78da2ac31cbb8c7642b73ff36342
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-08-21 02:39:22 +0530

    Fetch api data

--- comps/posts.tsx
+++ comps/posts.tsx
@@ -1,10 +1,12 @@
 import { P, H2 } from '../styles/comps/Posts.styled'
 
 const Posts = () => {
+  // console.log('launches', posts);
   return (
     <div>
       <P>Discover more about mens health</P>
       <H2>Explore the PocketMD Journal</H2>
+      {/* <h1>{posts[0].author}</h1> */}
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,65 @@ import styles from '../styles/Home.module.css'
 import Footer from '../comps/footer'
 import Hero from '../comps/hero'
 import Posts from '../comps/posts'
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
-const Home: NextPage = () => {
+interface PostsProps {
+  posts : {
+  author: string
+createdAt: string
+id: string
+score: number
+text: string
+title: string
+type: string
+updatedAt: string
+url: string
+__typename: string
+  }
+}
+
+
+const Home: NextPage<PostsProps> = (props) => {
+  const {posts}: PostsProps = props;
+  console.log(posts)
   return (
     <div className={styles.container}>
       <Header />
       <Hero />
-      <Posts />
+      <Posts/>
       <Footer />
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: 'https://gql-technical-assignment.herokuapp.com/graphql',
+    cache: new InMemoryCache()
+  });
+
+  const { data} = await client.query({
+    query: gql`
+      query {
+        firstPageArticles {
+          id
+          author
+          createdAt
+          score
+          updatedAt
+          title
+          text
+          type
+          url
+        }
+      }      
+    `
+  });
+  return {
+    props: {
+      posts: data.firstPageArticles
+    }
+  }
 }
 
 export default Home
