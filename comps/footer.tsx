af8288d7d397d1ae0b11b391ba9e1514a6da3e51
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-08-20 16:07:45 +0530

    Add footer logo

--- comps/footer.tsx
+++ comps/footer.tsx
@@ -1,7 +1,17 @@
 import { Foot } from '../styles/comps/Footer.styled'
+import Image from 'next/image'
 
 const Footer = () => {
-  return <Foot> Hello from footer</Foot>
+  return (
+  <Foot>
+     <Image
+            src="/logo.svg"
+            alt="hamburger"
+            width="100"
+            height="100"
+          />
+  </Foot>
+  )
 }
 
 export default Footer
--- styles/comps/Footer.styled.ts
+++ styles/comps/Footer.styled.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 
-const Foot = styled.footer``
+const Foot = styled.footer`
+  background-color: #000;
+`
 
 export { Foot }
