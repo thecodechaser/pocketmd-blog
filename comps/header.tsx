b917c51941d0f8cdd95aca05db01545c11fd43ed
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-08-20 15:28:44 +0530

    Add appointments button

--- comps/header.tsx
+++ comps/header.tsx
@@ -1,12 +1,27 @@
 import Link from 'next/link'
-import { Head } from '../styles/comps/Header.styled'
+import { Head, Button, Logo } from '../styles/comps/Header.styled'
+import Image from 'next/image'
 
 const Header = () => {
   return (
     <Head>
       <Link href="/">
-        <a>PocketMD Blog</a>
+        <Logo>PocketMD Blog</Logo>
       </Link>
+
+      {/* <div> */}
+        <Button>
+          APPOINTMENTS v
+        </Button>
+        <Link href="/">
+          <Image
+            src="/hamburger-button.png"
+            alt="hamburger"
+            width="35"
+            height="1"
+          />
+        </Link>
+      {/* </div> */}
     </Head>
   )
 }
--- styles/comps/Header.styled.ts
+++ styles/comps/Header.styled.ts
@@ -1,5 +1,24 @@
 import styled from 'styled-components'
 
-export const Head = styled.header`
-  font-size: 20px;
-`;
+const Head = styled.header`
+  display: flex;
+  margin: 10px 15px;
+  justify-content: space-between;
+`
+
+const Button = styled.button`
+  border-radius: 15px;
+  padding: 10px 10px;
+  box-shadow: 2px 2px 8px 2px rgba(0,0,0,0.2);
+  transition: 0.3s;
+  background-color: #fff;
+  border: 1px solid #fff;
+  font-weight: bold;
+`
+
+const Logo = styled.a`
+  font-size: 25px;
+  font-weight: bolder;
+`
+
+export { Head, Button, Logo }
