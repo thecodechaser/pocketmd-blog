86daad0419cb19336c47a0099e554942ffc4ec9e
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-08-21 00:03:54 +0530

    Add hero section texts

--- comps/hero.tsx
+++ comps/hero.tsx
@@ -1,7 +1,18 @@
-import { Div } from '../styles/comps/Hero.styled'
+import { Div, H1, P } from '../styles/comps/Hero.styled'
 
 const Hero = () => {
-  return <Div>Hello from hero</Div>
+  return (
+  <Div>
+
+    <div>
+      <H1>
+      Savoir Faire
+      </H1>
+      <P>it’s french. say it how it’s supposed to be said.
+        it’ll make your mouth feel funny.</P>
+    </div>
+  </Div>
+  )
 }
 
 export default Hero
--- styles/comps/Hero.styled.ts
+++ styles/comps/Hero.styled.ts
@@ -1,8 +1,21 @@
 import styled from 'styled-components'
 
 const Div = styled.div`
-background-color: #B7C7C9;
-height: 80vh;
+  display: flex;
+  background-color: #b7c7c9;
+  height: 80vh;
+  padding: 150px 120px;
 `
 
-export {Div};
+const H1 = styled.h1`
+font-size: 80px;
+`
+
+const P = styled.p`
+font-size: 25px;
+font-weight: 500;
+margin-top: -50px;
+width: 60%;
+`
+
+export { Div, H1, P }
