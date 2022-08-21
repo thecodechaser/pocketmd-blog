d26b5386d81a9308e75493096a7a3d72ae521b98
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-08-22 02:34:35 +0530

    Add responsivness for footer

--- styles/comps/Details.styled.ts
+++ styles/comps/Details.styled.ts
@@ -5,12 +5,18 @@ const H2 = styled.h2`
   font-size: 50px;
   font-weight: 600;
   text-align: center;
+  @media (max-width: 768px) {
+    font-size: 35px;
+  }
 `
 
 const H4 = styled.h4`
   font-size: 20px;
   font-weight: 500;
   text-align: center;
+  @media (max-width: 768px) {
+    font-size: 15px;
+  }
 `
 
 const P = styled.p`
